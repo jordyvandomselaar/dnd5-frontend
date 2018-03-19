aeb4ee0b586c7bea7e9d99e115bc27c74257796a
jordy <jordy.van.domselaar@outlook.com>
2018-03-19 12:36:50 +0100

    Add character show page

--- src/modules/characters/Module.js
+++ src/modules/characters/Module.js
@@ -1,10 +1,12 @@
 import React, {Fragment} from "react"
 import Index from "./views/Index"
 import {Route} from "react-router-dom"
+import Show from "./views/Show"
 
 const Module = () => (
     <Fragment>
         <Route exact path="/characters" component={Index}/>
+        <Route exact path="/characters/:id" component={Show}></Route>
     </Fragment>
 )
 
--- src/modules/characters/components/CharacterGrid/components/CharacterGridItem/index.js
+++ src/modules/characters/components/CharacterGrid/components/CharacterGridItem/index.js
@@ -1,11 +1,23 @@
 import React from "react"
 import {GridTile, GridTilePrimary, GridTilePrimaryContent, GridTileSecondary, GridTileTitle} from "rmwc/GridList/index"
+import {Link} from "react-router-dom"
+import styled from "styled-components"
+
+const CharacterImage = styled.img`
+  height: 100%;
+  width: 100%;
+  
+  object-fit: cover;
+`
+
 
 const CharacterGridItem = ({character}) => (
     <GridTile>
         <GridTilePrimary>
             <GridTilePrimaryContent>
-                <img src={character.image} alt="Character image"/>
+                <Link to={`/characters/${character.id}`}>
+                    <CharacterImage src={character.image} alt="Character image"/>
+                </Link>
             </GridTilePrimaryContent>
         </GridTilePrimary>
         <GridTileSecondary>
