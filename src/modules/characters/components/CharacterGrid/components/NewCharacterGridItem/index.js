00d1737a1916f392c66f4a7b91e080cb79e89892
jordy <jordy.van.domselaar@outlook.com>
2018-03-29 21:45:10 +0200

    Add character button actually navigates now.

--- src/modules/characters/components/CharacterGrid/components/NewCharacterGridItem/index.js
+++ src/modules/characters/components/CharacterGrid/components/NewCharacterGridItem/index.js
@@ -1,39 +1,45 @@
 // @flow
 
-import type {Element} from "react"
-import React from "react"
-import {GridTile, GridTilePrimary, GridTilePrimaryContent} from "rmwc/GridList"
-import {Ripple} from "rmwc/Ripple"
-import {Icon} from "rmwc/Icon"
-import styled from "styled-components"
+import type {Element} from "react";
+import React from "react";
+import {GridTile, GridTilePrimary, GridTilePrimaryContent} from "rmwc/GridList";
+import {Ripple} from "rmwc/Ripple";
+import {Icon} from "rmwc/Icon";
+import styled from "styled-components";
+import Link from "react-router-dom/es/Link";
 
-const IconContainer = styled.div`
+const StyledLink = styled(Link)`
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  
   display: flex;
   justify-content: center;
   align-items: center;
   
-  & > i {
+  text-decoration: none;
+  
+  & i {
       font-size: 60px;
       pointer-events: none;
+      color: #000;
   }
-  
-  &:hover {
-    cursor: pointer;
-  }
-`
+`;
 
 const NewCharacterGridItem = (): Element<GridTile> => (
     <GridTile>
         <Ripple>
             <GridTilePrimary>
                 <GridTilePrimaryContent>
-                    <IconContainer>
+                    <StyledLink to="/characters/new">
                         <Icon>add</Icon>
-                    </IconContainer>
+                    </StyledLink>
                 </GridTilePrimaryContent>
             </GridTilePrimary>
         </Ripple>
     </GridTile>
-)
+);
 
-export default NewCharacterGridItem
+export default NewCharacterGridItem;
